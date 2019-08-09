64b22a7ca8de8c9d8a1ac44e84f0218fdc422cb4
Ioana-Postolache <ioana.postolache.zaharia@gmail.com>
2019-08-09 08:32:11 +0000

    Data partitioning

--- airflow/dags/lesson2_exercises/exercise3.py
+++ airflow/dags/lesson2_exercises/exercise3.py
@@ -11,7 +11,7 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.operators.postgres_operator import PostgresOperator
 from airflow.operators.python_operator import PythonOperator
 
-import sql_statements
+import sql_statements2
 
 
 def load_trip_data_to_redshift(*args, **kwargs):
